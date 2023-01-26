fbefb4185f101042d220accd16507a11a4e8dfb2
TaeSeok Jung <wjdxotjr383@naver.com>
2023-01-26 19:11:20 +0900

    [level 1] Title: 제일 작은 수 제거하기, Time: 0.08 ms, Memory: 74 MB -BaekjoonHub

---
+++ 프로그래머스/lv1/12935. 제일 작은 수 제거하기/제일 작은 수 제거하기.java
@@ -0,0 +1,24 @@
+class Solution {
+    public int[] solution(int[] arr) {
+        if(arr.length==1){
+            int[] answer = {-1};
+            return answer;
+        }
+        int[] answer = new int[arr.length-1];     
+        int min = arr[0];
+        
+        for(int i =0; i<arr.length; i++){
+            if(arr[i]<=min){
+                min = arr[i];
+            }
+        }
+        int cnt = 0;
+        for(int i=0; i<arr.length; i++){
+            if(arr[i]==min){
+                continue;
+            }
+            answer[cnt++] = arr[i];
+        }
+        return answer;
+    }
+}
