e00ff84af714c8e5b45dde83931fd4ffae824cf6
TaeSeok Jung <wjdxotjr383@naver.com>
2023-02-08 19:01:29 +0900

    [unrated] Title: 명예의 전당 (1), Time: 0.22 ms, Memory: 84.8 MB -BaekjoonHub

---
+++ 프로그래머스/unrated/138477. 명예의 전당 （1）/명예의 전당 （1）.java
@@ -0,0 +1,28 @@
+import java.util.*;
+
+class Solution {
+    public int[] solution(int k, int[] score) {
+        int[] answer = new int[score.length];
+        ArrayList<Integer> list = new ArrayList<>();
+        for(int i=0; i<score.length; i++){
+            if(list.size()<k){
+                list.add(score[i]);
+                Collections.sort(list);
+                answer[i] = list.get(0);
+                continue;
+            }
+            if(list.size()==k){
+                int min = list.get(0);
+                if(min <score[i]){
+                    list.remove(0);
+                    list.add(score[i]);
+                    Collections.sort(list);
+                }
+                answer[i] = list.get(0);
+            }
+        }
+
+        
+        return answer;
+    }
+}
