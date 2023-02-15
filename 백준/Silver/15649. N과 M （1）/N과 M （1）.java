0e034b5ec7ad3ffb46f7a188cc8ee9fcd3201e94
TaeSeok Jung <wjdxotjr383@naver.com>
2023-02-15 19:08:12 +0900

    [Silver III] Title: N과 M (1), Time: 2924 ms, Memory: 60896 KB -BaekjoonHub

---
+++ 백준/Silver/15649. N과 M （1）/N과 M （1）.java
@@ -0,0 +1,33 @@
+import java.util.*;
+
+public class Main {
+    public static int[] arr;
+    public static boolean[] visit;    // 방문여부
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        
+        int n = sc.nextInt();
+        int m = sc.nextInt();
+        
+        arr = new int[m];
+        visit = new boolean[n];
+        dfs(n,m,0);
+    }
+    public static void dfs(int n, int m, int depth){
+        if(depth == m){
+            for(int val : arr){
+                System.out.print(val+" ");
+            }
+            System.out.println();
+            return;
+        }
+        for(int i = 0; i<n; i++){
+            if(!visit[i]){
+                visit[i] = true;
+                arr[depth] = i+1;
+                dfs(n,m,depth+1);
+                visit[i] = false;
+            }
+        }
+    }
+}
