3f03f7ccbdb87338f67797c9c3eda7c269d23a5f
TaeSeok Jung <wjdxotjr383@naver.com>
2023-02-21 17:14:11 +0900

    [Bronze I] Title: 초콜릿 자르기, Time: 212 ms, Memory: 17708 KB -BaekjoonHub

---
+++ 백준/Bronze/2163. 초콜릿 자르기/초콜릿 자르기.java
@@ -0,0 +1,15 @@
+import java.util.*;
+import java.io.*;
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+       int n = sc.nextInt();
+        int m = sc.nextInt();
+        int count = 0;
+        
+        count = m*n-1;
+        
+        System.out.print(count);
+    }
+}
