1994c28f6da7ec206a9b92c79bc691fb74c5280e
TaeSeok Jung <wjdxotjr383@naver.com>
2023-03-20 18:59:46 +0900

    [level 2] Title: 주식가격, Time: 13.02 ms, Memory: 66.1 MB -BaekjoonHub

---
+++ 프로그래머스/lv2/42584. 주식가격/주식가격.java
@@ -0,0 +1,20 @@
+class Solution {
+    public int[] solution(int[] prices) {
+        int[] answer = new int[prices.length];
+        answer[prices.length-1] = 0;
+        for(int i=0; i<prices.length-1; i++){
+            for(int j=i+1; j<prices.length; j++){
+                if(prices[i]<=prices[j]){
+                    answer[i]++;
+                    
+                }
+                else {
+                    answer[i]++;
+                    break;
+                }
+            }
+        }
+        
+        return answer;
+    }
+}
