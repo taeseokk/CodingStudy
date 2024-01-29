ca0f1935a78a71a5e30ddb6f2a488c3443166785
TaeSeok Jung <wjdxotjr383@naver.com>
2024-01-29 18:55:45 +0900

    [level 3] Title: 등굣길, Time: 0.47 ms, Memory: 52.6 MB -BaekjoonHub

---
+++ 프로그래머스/3/42898. 등굣길/등굣길.java
@@ -0,0 +1,27 @@
+class Solution {
+	public int solution(int m, int n, int[][] puddles) {
+		int[][] dp = new int[n][m];
+		for (int[] puddle : puddles)
+			dp[puddle[1] - 1][puddle[0] - 1] = -1;
+		dp[0][0] = 1;
+		for (int i = 0; i < n; i++) {
+			for (int j = 0; j < m; j++) {
+				if (dp[i][j] == -1) {
+					dp[i][j] = 0;
+					continue;
+				}
+				if (i != 0) {
+					dp[i][j] += dp[i - 1][j]% 1000000007;
+				}
+
+				if (j != 0) {
+					dp[i][j] += dp[i][j - 1]% 1000000007;
+				}
+
+			}
+
+		}
+		int answer = dp[n - 1][m - 1] % 1000000007;
+		return answer;
+	}
+}
