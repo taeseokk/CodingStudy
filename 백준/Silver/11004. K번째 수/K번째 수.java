13b9368461bc73592015845389fb23adb1e36e20
TaeSeok Jung <wjdxotjr383@naver.com>
2024-03-18 16:53:29 +0900

    [Silver V] Title: K번째 수, Time: 2140 ms, Memory: 377544 KB -BaekjoonHub

---
+++ 백준/Silver/11004. K번째 수/K번째 수.java
@@ -0,0 +1,27 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.Arrays;
+import java.util.StringTokenizer;
+
+
+public class Main {
+
+	public static void main(String[] args) throws IOException {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		StringTokenizer st = new StringTokenizer(br.readLine());
+		
+		int k = Integer.parseInt(st.nextToken());
+		int n = Integer.parseInt(st.nextToken());
+		
+		int[] arr = new int[k];
+		
+		st = new StringTokenizer(br.readLine());
+		for(int i=0; i<k; i++) {
+			arr[i] = Integer.parseInt(st.nextToken());
+		}
+		
+		Arrays.sort(arr);
+		System.out.println(arr[n-1]);
+	}
+}
