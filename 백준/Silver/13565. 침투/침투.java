5fad0af1fa3ab15adea95be7730b2c28e9c400b1
TaeSeok Jung <wjdxotjr383@naver.com>
2024-04-24 19:44:50 +0900

    [Silver II] Title: 침투, Time: 468 ms, Memory: 80224 KB -BaekjoonHub

--- 백준/Silver/13565. 침투/침투.java
+++ 백준/Silver/13565. 침투/침투.java
@@ -1,6 +1,8 @@
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
+import java.util.LinkedList;
+import java.util.Queue;
 import java.util.StringTokenizer;
 
 public class Main {
@@ -8,6 +10,7 @@ public class Main {
 	static int[] Y = { 1, -1, 0, 0 };
 	static int[][] map;
 	static boolean[][] visited;
+	static Queue<Point> que;
 	static int m, n;
 	static String answer;
 
@@ -22,6 +25,7 @@ public class Main {
 		map = new int[m][n];
 		visited = new boolean[m][n];
 		answer = "NO";
+		que = new LinkedList<>();
 
 		for (int i = 0; i < m; i++) {
 			String s = br.readLine();
@@ -31,37 +35,51 @@ public class Main {
 		}
 
 		for (int i = 0; i < n; i++) {
-			if (map[0][i] == 0 && !visited[0][i]) {
-				dfs(i, 0);
+			if (!visited[0][i] && map[0][i] == 0) {
+				que.offer(new Point(i, 0));
 			}
 		}
+		bfs();
 
 		System.out.println(answer);
 
 	}
 
-	static void dfs(int x, int y) {
-		visited[y][x] = true;
+	static void bfs() {
+		while (!que.isEmpty()) {
+			Point pt = que.poll();
+			int nowX = pt.x;
+			int nowY = pt.y;
 
-		if (y == m - 1) {
-			answer = "YES";
-			return;
-		}
-
-		for (int i = 0; i < 4; i++) {
-			int newX = x + X[i];
-			int newY = y + Y[i];
-
-			if (check(newX, newY) && !visited[newY][newX] && map[newY][newX] == 0) {
-
-				dfs(newX, newY);
-
+			if (nowY == m - 1) {
+				answer = "YES";
+				return;
 			}
+			visited[nowY][nowX] = true;
+			for (int i = 0; i < 4; i++) {
 
+				int newX = pt.x + X[i];
+				int newY = pt.y + Y[i];
+
+				if (check(newX, newY) && !visited[newY][newX] && map[newY][newX] == 0) {
+					que.offer(new Point(newX, newY));
+					visited[newY][newX] = true;
+				}
+			}
 		}
 	}
 
 	static boolean check(int x, int y) {
 		return (x >= 0 && y >= 0 && x < n && y < m);
+	}
+}
+
+class Point {
+	int x;
+	int y;
+
+	Point(int x, int y) {
+		this.x = x;
+		this.y = y;
 	}
 }
