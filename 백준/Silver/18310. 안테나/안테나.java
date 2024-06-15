a77b9d841f95f01cb7215b160ce742a5e1fd2c15
TaeSeok Jung <wjdxotjr383@naver.com>
2024-06-15 16:44:08 +0900

    [Silver III] Title: 안테나, Time: 588 ms, Memory: 40252 KB -BaekjoonHub

---
+++ 백준/Silver/18310. 안테나/안테나.java
@@ -0,0 +1,24 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.*;
+
+public class Main {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st;
+
+        int n = Integer.parseInt(br.readLine());
+        int[] arr = new int[n];
+
+        st = new StringTokenizer(br.readLine());
+        for(int i=0; i<n; i++){
+            arr[i] = Integer.parseInt(st.nextToken());
+        }
+        Arrays.sort(arr);
+
+       int answer = arr[(n-1)/2];
+
+        System.out.println(answer);
+    }
+}
