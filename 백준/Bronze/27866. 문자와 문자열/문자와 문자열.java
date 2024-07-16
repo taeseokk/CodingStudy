67dc411c33fba35dafb47f115dd38481caddcc1e
TaeSeok Jung <wjdxotjr383@naver.com>
2024-07-16 17:36:03 +0900

    [Bronze V] Title: 문자와 문자열, Time: 104 ms, Memory: 14360 KB -BaekjoonHub

---
+++ 백준/Bronze/27866. 문자와 문자열/문자와 문자열.java
@@ -0,0 +1,24 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.*;
+
+public class Main {
+
+
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringBuilder sb = new StringBuilder();
+        StringTokenizer st;
+
+        String s = br.readLine();
+
+        int n = Integer.parseInt(br.readLine());
+
+        Character ch = s.charAt(n-1);
+
+        System.out.println(ch);
+
+    }
+
+}
